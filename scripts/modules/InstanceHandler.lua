119a1bbe9ddffa9ff8bda0b2f2a880e4ce94fd81
68656c702e <ava.flecher@protonmail.com>
2021-06-22 17:20:31 -0400

    Initial commit

--- scripts/modules/InstanceHandler.lua
+++ scripts/modules/InstanceHandler.lua
@@ -7,6 +7,7 @@ function module.Instances:CreatePart(Parent, Color, Size, Anchored)
     Part.Color = Color or Color3.fromRGB(0,0,0)
     Part.Size = Size or Vector3.new(4,1,2)
     Part.Anchored = Anchored or true
+    Part.Parent = Parent or script
 
     return Part
 end
