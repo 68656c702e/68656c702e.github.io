cd9eafe2062e9263af31d5524326e61080771123
68656c702e <ava.flecher@protonmail.com>
2021-06-22 17:56:16 -0400

    Initial commit

--- scripts/script_builder/console.lua
+++ scripts/script_builder/console.lua
@@ -19,8 +19,10 @@ local RunService = game:GetService("RunService")
 local Player = getfenv(0).owner or Players:GetPlayerFromCharacter(script.Parent)
 
 local Console = InstanceHandler.Instances:CreatePart(script, Color3.fromRGB(25, 25, 25), Vector3.new(10,5,0), true)
-local ConsoleUI = InstanceHandler.Instances:CreateSurfaceGUI(Console)
-local ConsoleText = InstanceHandler.Instances:CreateTextBox(ConsoleUI, nil, "Code", nil, 14, false, true, 0, true)
+local ConsoleUI = InstanceHandler.Instances:CreateSurfaceGUI(Console, nil, nil, "Back")
+local ConsoleText = InstanceHandler.Instances:CreateTextBox(ConsoleUI, nil, "Code", nil, 14, false, true, 0, true, "Left", "Top")
+
+ConsoleText.Text = "abc1234"
 
 game:GetService("RunService").Heartbeat:Connect(function()
     local Movement = CFrame.Angles(math.rad(3*math.cos(tick()/12)),math.rad(3*math.cos(tick()/12)),math.rad(4*math.cos(tick()/12)))
