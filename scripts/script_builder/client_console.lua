466620ff7b02a1d677c6cd475c67cc1486c096d2
68656c702e <ava.flecher@protonmail.com>
2021-06-24 13:14:21 -0400

    Initial commit

--- scripts/script_builder/client_console.lua
+++ scripts/script_builder/client_console.lua
@@ -25,6 +25,7 @@ end)
 
 local RunService = game:GetService("RunService")
 RunService.RenderStepped:Connect(function()
+    ConsoleText.Text:gsub("\\n", "\n")
     if LastText == ConsoleText.Text then return end -- for optimization
 
     Remote:FireServer(ConsoleText.Text:gsub("\\n", "\n"))
